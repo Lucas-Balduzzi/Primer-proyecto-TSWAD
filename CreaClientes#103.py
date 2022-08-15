2c2f65aee7656adb909f8494039e7a6c6b3d804e
Lucas-Balduzzi <106196812+Lucas-Balduzzi@users.noreply.github.com>
2022-08-14 22:59:04 -0300

    Add files via upload

---
+++ CreaClientes#103.py
@@ -0,0 +1,6 @@
+# 10) Crear lista Clientes, Nombres de Dueños de perros 
+Clientes=["Juan","Mario","Ariel","Josefina","Marianella"]
+print(Clientes)
+  
+print("Lista de Clientes ordenada alfabéticamente", sorted(Clientes))        
+  
